7405b25d6eb5c39502e3bd3901ea29009cc96186
Haseeb U <haseebabbasi00@gmail.com>
2022-01-11 19:31:15 +0500

    back end module partially completed

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 const express  = require('express')
-const cookieParser = require('cookie-parser')
 const app = new express()
 const errorMiddleware = require('./middlewares/errors')
-const userRouter = require('./routes/UserRoutes')
+const userRouter = require('./routes/PersonRoutes')
+const eventRouter = require('./routes/EventRoutes')
+const taskRouter = require('./routes/TaskRoutes')
+const notesRouter = require('./routes/NotesRoutes')
 
 app.use(express.json())
-app.use(cookieParser())
 
 app.use(userRouter)
+app.use(notesRouter)
+app.use(eventRouter)
+
 
 module.exports = app;
--- backend/routes/UserRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllUsers } = require('../controllers/UserController');
-
-router.route('/users').get(getAllUsers);
-
-module.exports = router ;
