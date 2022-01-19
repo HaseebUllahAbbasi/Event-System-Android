135ff3b47c7de6c347148adee3a2cee85b591d8a
Arslan Ahmed <arslanahmed.kcsf18@iba-suk.edu.pk>
2022-01-19 21:50:03 +0500

    animation added

--- drawer_activity/app/src/main/java/com/example/drawer_activity/LogoScreen.java
+++ drawer_activity/app/src/main/java/com/example/drawer_activity/LogoScreen.java
@@ -6,15 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class LogoScreen extends AppCompatActivity {
+public class LogoScreen extends AppCompatActivity  implements Animation.AnimationListener{
 
+    ImageView imageView;
+    View view;
+    Animation animation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo_screen);
+
+         animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.myanimation);
+        imageView = findViewById(R.id.imageView3);
+
+        animation.setAnimationListener(this);
+
+        imageView.setVisibility(View.VISIBLE);
+
+        imageView.startAnimation(animation);
+
+
         Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        handler.postDelayed(new Runnable()
+        {
             @Override
             public void run() {
 
@@ -29,5 +49,20 @@ public class LogoScreen extends AppCompatActivity {
 
     public void HomeScreen(View view)
     {
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
