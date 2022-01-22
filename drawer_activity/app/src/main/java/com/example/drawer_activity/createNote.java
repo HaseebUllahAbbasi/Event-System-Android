51df78a97d018eea061c01ae56bb6b9a2c09a29f
Haseeb U <haseebabbasi00@gmail.com>
2022-01-22 16:10:12 +0500

    more screens added

---
+++ drawer_activity/app/src/main/java/com/example/drawer_activity/createNote.java
@@ -0,0 +1,29 @@
+package com.example.drawer_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class createNote extends AppCompatActivity {
+
+    private static final String TAG =  "Note";
+    TextInputEditText note_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+        note_text = findViewById(R.id.note_text);
+
+    }
+
+    public void createNote(View view)
+    {
+        Log.d(TAG, "createNote: "+note_text.getText().toString());
+
+    }
+}
