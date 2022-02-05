dd0af1c9fcc6201ba03cd2c3ad5c359125bb1107
Haseeb U <haseebabbasi00@gmail.com>
2022-02-05 20:38:43 +0500

    changes in the profile  screen

---
+++ drawer_activity/app/src/main/java/com/example/drawer_activity/ModifyProfile.java
@@ -0,0 +1,17 @@
+package com.example.drawer_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifyProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_profile);
+
+    }
+}
