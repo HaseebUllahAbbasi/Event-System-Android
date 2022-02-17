270a9499b8e62fab0bb2fe28ed301c515d6635e6
Haseeb Ullah <53255366+HaseebUllahAbbasi@users.noreply.github.com>
2022-02-17 16:27:11 +0500

    changes to be made

--- links_to_use.js
+++ links_to_use.js
@@ -12,3 +12,13 @@ https://www.spaceotechnologies.com/best-android-libraries/
 
 https://github.com/CuriousNikhil/neumorphic-compose
 */
+
+// onRestoredInstance Method to be implmented when new note or task is created
+// change the color scheme
+// initially show the loading screen or activity indicator 
+// create custom notification
+// save the profile picture in local storage 
+// save the profile picture on 
+// change the deisgn
+// seprate the events ; my events and invited events
+// home screen should be better;
